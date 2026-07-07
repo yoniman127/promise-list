10aa01c06126e2257f9c204d54c202438ddd8afb
yoniman127 <danielyonathan745@gmail.com>
2026-07-07 14:31:18 +0300

    Project: Sleek to-do List Website

---
+++ promise.js
@@ -0,0 +1,107 @@
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const todoListUL = document.getElementById('todo-list');
+
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    addTodo();
+})
+
+function addTodo(){
+    const todoText = todoInput.value.trim();
+    if(todoText.length > 0){
+        const todoObject = {
+            text: todoText,
+            completed: false
+        }
+        allTodos.push(todoObject);
+        updateTodoList();
+        saveTodos();
+        todoInput.value = "";
+    }  
+}
+function updateTodoList(){
+    todoListUL.innerHTML = "";
+    allTodos.forEach((todo, todoIndex)=>{
+        todoItem = createTodoItem(todo, todoIndex);
+        todoListUL.append(todoItem);
+    })
+}
+function createTodoItem(todo, todoIndex){
+    const todoId = "todo-"+todoIndex;
+    const todoLI = document.createElement("li");
+    const todoText = todo.text;
+    todoLI.className = "todo";
+    todoLI.innerHTML = `
+        <input type="checkbox" id="${todoId}">
+        <label class="custom-checkbox" for="${todoId}">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z"/></svg>
+        </label>
+        <label for="${todoId}" class="todo-text">
+            ${todoText}
+        </label>
+        <button class="delete-button">
+            <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+        </button>
+    `
+    const deleteButton = todoLI.querySelector(".delete-button");
+    deleteButton.addEventListener("click", ()=>{
+        deleteTodoItem(todoIndex);
+    })
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener("change", ()=>{
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+    })
+    checkbox.checked = todo.completed;
+    return todoLI;
+}
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
+    saveTodos();
+    updateTodoList();
+}
+function saveTodos(){
+    const todosJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos", todosJson);
+}
+function getTodos(){
+    const todos = localStorage.getItem("todos") || "[]";
+    return JSON.parse(todos);
+}
+// EXTRAS
+
+const arrays = ["make promises then keep them","only you are capable of your future","risk is better than regret",]
+let index = 0
+function placeHolder(){
+    todoInput.placeholder = arrays[index]
+    index = (index + 1) % arrays.length
+}
+setInterval(placeHolder, 4500)
+placeHolder()
+
+const theme = document.getElementById("mytheme")
+const about = document.getElementById("about")
+const dialog = document.getElementById("about-alert")
+const closeAbout = document.getElementById("Close")
+
+const saveTheme = localStorage.getItem("theme")
+if(saveTheme === "light"){
+    document.documentElement.classList.add("dark-theme")
+}
+theme.addEventListener("click", darkmode)
+
+function darkmode(){
+    const isDarkTheme = document.documentElement.classList.toggle("dark-theme")
+    localStorage.setItem("theme", isDarkTheme ? "light" : "dark")
+}
+about.addEventListener("click", () => {
+    dialog.showModal()
+})
+closeAbout.addEventListener("click", () => {
+    dialog.close()
+})
+
